7f5f017bddffef46759eb2488e7de49ca84decd6
lostfromlight1 <kaungthanlwinkyaw@gmail.com>
2026-02-02 11:36:01 +0630

    update

--- src/com/talent/java/day5/school/School.java
+++ src/com/talent/java/day5/school/School.java
@@ -3,15 +3,15 @@ package com.talent.java.day5.school;
 public class School {
     public static void main(String[] args) {
 
-        Person student = new Student("Aung", 16, 10);
-        Person teacher = new Teacher("Ms. May", 35, "Math");
+        Person[] people = {
+                new Student("Aung", 16, 10),
+                new Teacher("Ms. May", 35, "Math")
+        };
 
-        student.introduce();
-        System.out.println("Role: " + student.getRole());
-        System.out.println();
-
-        teacher.introduce();
-        System.out.println("Role: " + teacher.getRole());
-        System.out.println();
+        for (Person person : people) {
+            person.introduce();
+            System.out.println("Role: " + person.getRole());
+            System.out.println();
+        }
     }
 }
